d7b1ed4d8fe223342b684a577b8cac26d75e7558
ice cube tray <icecubetray@users.noreply.github.com>
2020-01-03 15:08:20 -0500

    Add parseCmdline

--- src/Core/Console.lua
+++ src/Core/Console.lua
@@ -13,3 +13,55 @@ end
 
 kcdfw.logNotice("Logging function upgraded.");
 kcdfw.logBootstrap("Permitted log levels set to %u to %u.", KCDFW_LEVEL_BOOTSTRAP, kcdfw.logLevel);
+
+
+
+
+kcdfw.normalizeCmdlineKey = function(key)
+	return key:gsub("^-*", "");
+end
+
+kcdfw.parseCmdline = function(cmdline, nonoptions)
+	local found = {};
+	local nopts = (nonoptions or {});
+
+	local key = nil;
+	local optend = false;
+	for arg in cmdline:gmatch("%S+") do
+		if not optend then
+			if arg:sub(1, 1) == '-' then
+				if key then
+					found[key] = true
+				end
+
+				if arg == '--' then
+					key = nil;
+					optend = true;
+				else
+					key = kcdfw.normalizeCmdlineKey(arg);
+				end
+			else
+				if key then
+					found[key] = arg;
+					key = nil;
+				else
+					table.insert(nopts, arg);
+				end
+			end
+		else
+			table.insert(nopts, arg);
+		end
+	end
+
+	if key then
+		found[key] = true
+		key = nil;
+	end
+
+	-- For any key=value pairs, override.
+	for key, value in cmdline:gsub(" -- .*", ""):gmatch("-?(%S+)=(%S+)") do
+		found[kcdfw.normalizeCmdlineKey(key)] = value;
+	end
+
+	return found;
+end
